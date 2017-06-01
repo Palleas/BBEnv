210f77b5b5d0eb0e7ef12257fa45623f2c3f85e0
Romain Pouclet <romain.pouclet@gmail.com>
2017-05-31 21:13:56 -0400

    Support launching multiple steps

--- run.js
+++ run.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
+// @flow
 
 const _ = require('lodash');
 
 ((launchArgs) => {
     if (launchArgs.length < 3) {
-        console.log("usage: bbenv buddybuild_script.sh");
+        console.log("usage: bbenv steps...");
         process.exit(2); // TODO: is this the proper exit code? We'll never know!
     }
 
-    const script = _.nth(launchArgs, 2);
+    const steps = _.slice(launchArgs, 2);
 
-    require('./lib/main')
-    .exec(script)
-    .then(output => console.log(output));
+    require('./lib/main').exec(steps)
 })(process.argv);
